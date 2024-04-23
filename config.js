1b7a57a21c2eba9ee3f92711d9a36104910e18f5
Slepalonix <161464589+slepalonix@users.noreply.github.com>
2024-04-23 21:13:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,11 +8,7 @@ import axios from 'axios'
 
 global.owner = [
   ['255614112247', 'khuzaima ', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
-  ['233531910067', 'Abraham Dwamena3', true],
+  
   
 ] //Number of owners
 
@@ -57,8 +53,8 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = 'slepalonix '
 global.premium = 'true'
-global.packname = 'XLICON khuzaima ' 
-global.author = 'slepalonix ' 
+global.packname = 'slepalonix 💔' 
+global.author = 'slepalonix 💔' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
@@ -69,8 +65,8 @@ global.fglog = 'XLICON.jpg'
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
 
-global.wait = '*🕣 _XLICON IS LOADIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
-global.rwait = '⌛'
+global.wait = 'please wait slepalonix my owner *'
+global.rwait = '💃'
 global.dmoji = '🤭'
 global.done = '✔️'
 global.error = '❌' 
